ad012c0a76872232adcf7dff5c4904161953d3f7
hyd <hyd@hydde-MacBook-Pro.local>
2018-11-08 14:59:45 +0800

    change

--- js/demo/setting.js
+++ js/demo/setting.js
@@ -1,18 +1,18 @@
     var mapboxTiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>', maxZoom: 18,})
     var map = L.map('map')
           .addLayer(mapboxTiles)
-          .setView([22.287111, 114.191667],11);
+          .setView([22.287111, 114.191667],9);
     var items = [];
     var airtable_read_endpoint = "https://api.airtable.com/v0/appw2nhwOdq8ewe2j/%E5%85%8D%E7%A8%85%E9%85%92%E8%B3%BC%E8%B2%B7%E8%81%96%E5%9C%B0%28YELP%E6%8E%A8%E8%96%A6%29?api_key=keyV24tV2nJhgN75C";
     var data = [];
     var LeafIcon = L.Icon.extend({
       options: {
-        shadowUrl: 'wine.png',
+        shadowUrl: 'icon.png',
         iconSize:     [35, 40],
         shadowSize:   [25, 26],
       }
     });
-    var greenIcon = new LeafIcon({iconUrl: 'wine.png'});
+    var wineIcon = new LeafIcon({iconUrl: 'icon.png'});
 
     $.getJSON(airtable_read_endpoint, function(result) {
            $.each(result.records, function(key,value) {
@@ -31,6 +31,6 @@
 function show_districts(){
       for (var i in data) {
           var latlng = L.latLng({ lat: data[i].Lat, lng: data[i].Lng});
-          L.marker( latlng,{icon:greenIcon})
+          L.marker( latlng,{icon:wineIcon})
               .bindPopup( '<img src="' + data[i].image_url+'" width = "80px+" padding="5px"><br>'+'<strong>'+ data[i].name+ '</strong>'+"<br>"+data[i].genre+"<br>"+data[i].contact+"<br>"+data[i].address)
               .addTo(map)}};
